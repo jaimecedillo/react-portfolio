8bb251f9463c71077ebd3d306ba6509a32151449
Jaime Cedillo <74879881+jaimecedillo@users.noreply.github.com>
2021-06-05 11:49:34 -0600

    added About component

---
+++ src/components/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import myImage from '../../src/assets/jaime.jpg'
+
+function About() {
+  return (
+    <section className="my-5">
+      <h1 id='about'>About Me</h1>
+
+      <img src={myImage} className="bio-image" alt="jaime" />
+
+      <div className="my-2">
+        <h2>Jaime Cedillo <br /> Web Developer</h2>
+        <p>
+          Hi my name is Jaime, I'm based in Utah, USA. I have skills on HTML, CSS, JavaScript, jQuery, AJAX and
+          GIT. If you're an employer looking for a Web Developer, feel free to look at my work and get in touch with me.
+         </p>
+     
+    
+      </div >
+     </section >
+  );
+}
+
+export default About;
