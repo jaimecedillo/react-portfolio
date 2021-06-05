01493d9c2657f37f54535a0f2df1af43100d90d3
Jaime Cedillo <74879881+jaimecedillo@users.noreply.github.com>
2021-06-05 15:27:46 -0600

    contact form component and styling

--- src/App.js
+++ src/App.js
@@ -1,42 +1,43 @@
-import React, { useState } from 'react';
-import About from './components/About'
-import Nav from './components/Nav'
+import React from 'react';
+// import About from './components/About'
+// import Nav from './components/Nav'
 import Resume from './components/Resume'
-import Portfolio from './components/Portfolio'
-import Contact from './components/Contact'
+// import Portfolio from './components/Portfolio'
+// import Contact from './components/Contact'
 
 import './App.css';
 import './style/style.css'
 
 
 function App() {
-  const [currentPage, handlePageChange] = useState('About');
+  // const [currentPage, handlePageChange] = useState('About');
 
-  const renderPage = () => {
-    // Add a switch statement that will return the appropriate component of the 'currentPage'
-    switch (currentPage) {
-      case 'About':
-        return <About />;
-      case 'Portfolio':
-        return <Portfolio />;
-      case 'Contact':
-        return <Contact />;
-      case 'Resume':
-        return <Resume />;
-      default: 'About';
-        return <About />
-    }
+  // const renderPage = () => {
+  //   // Add a switch statement that will return the appropriate component of the 'currentPage'
+  //   switch (currentPage) {
+  //     case 'About':
+  //       return <About />;
+  //     case 'Portfolio':
+  //       return <Portfolio />;
+  //     case 'Contact':
+  //       return <Contact />;
+  //     case 'Resume':
+  //       return <Resume />;
+  //     default: 'About';
+  //       return <About />
+  //   }
 
-  };
+  // };
   return (
     <div>
-      <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* <Nav currentPage={currentPage} handlePageChange={handlePageChange} /> */}
       <main>
-        {
+        <Resume />
+        {/* {
           // Render the component returned by 'renderPage()'
           renderPage()
           //
-        }
+        } */}
       </main>
     </div>
   );
